bbb24b4f89f4b42159d0ea45084886707b29e668
Kenneth Siu <kenneth.siu@softwire.com>
2021-10-14 15:34:08 +0100

    Can connect to database and retrieve all books

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require("express");
+const pgp = require("pg-promise")();
+const connectionString = require("./config.js");
+
+const db = pgp(connectionString);
 
 const app = express();
+
+app.get("/books", (req, res) => {
+    db.any("SELECT * FROM book")
+        .then(function (data) {
+            res.send(data);
+        })
+        .catch(function (error) {
+            res.status(500);
+            console.error(error);
+        });
+});
 
 app.listen(3000, () => {
     console.log("Bookish is listening on port 3000");
